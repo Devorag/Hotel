11cffd1f8886958f59add7e1ea4ea6b54e2f1924
Devorag <141979085+Devorag@users.noreply.github.com>
2023-12-21 00:43:04 -0500

    Update 4 reports.sql
    added a space

--- 4 reports.sql
+++ 4 reports.sql
@@ -14,7 +14,7 @@ from guests g
 order by g.TotalCostOfStay desc
 --5. for govt purposes, we need a column in this format:
 --first name last name: amount of days stayed, total price paid (Hotel Location)
-select ConcatFormat = concat(g.firstname, '', g.lastname, ': ', g.numdaysstayed, ', ', g.TotalCostOfStay, ' (', g.hotelLocation, ')')
+select ConcatFormat = concat(g.firstname, ' ', g.lastname, ': ', g.numdaysstayed, ', ', g.TotalCostOfStay, ' (', g.hotelLocation, ')')
 from guests g 
 --6. show me the avg amount of days guests stayed
 select AverageNumDaysStayed = avg(numdaysstayed)
